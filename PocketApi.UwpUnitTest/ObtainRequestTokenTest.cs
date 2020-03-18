cac51c24bfb153742118f95a5a230f2a16194a73
Nick Ward <nickward13@hotmail.com>
2020-03-18 19:27:52 +1100

    added test for ObtainRequestToken

--- PocketApi.UwpUnitTest/ObtainRequestTokenTest.cs
+++ PocketApi.UwpUnitTest/ObtainRequestTokenTest.cs
@@ -4,12 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
 
 namespace PocketApi.UwpUnitTest
 {
     [TestClass]
     public class ObtainRequestTokenTest
     {
+        [TestMethod]
+        public async Task ExecuteTest()
+        {
+            RequestToken requestToken = await ObtainRequestToken.ExecuteAsync(
+                WebAuthenticationBroker.GetCurrentApplicationCallbackUri(),
+                Secrets.PocketAPIConsumerKey);
 
+            Assert.IsNotNull(requestToken);
+            Assert.IsNotNull(requestToken.Code);
+            Assert.AreEqual(30, requestToken.Code.Length);
+        }
     }
 }
