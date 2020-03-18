5415d5898b04b060bb92054ec1022740b5ead8c0
Nick Ward <nickward13@hotmail.com>
2020-03-18 19:14:09 +1100

    Add project files.

---
+++ PocketApi/RequestTokenRequestBody.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PocketApi
+{
+    class RequestTokenRequestBody
+    {
+        [JsonPropertyName("consumer_key")]
+        public string ConsumerKey { get; set; }
+        [JsonPropertyName("redirect_uri")]
+        public string RedirectUri { get; set; }
+    }
+}
