cde4382d5ba9635a7b2fbc0f2ee2ad628bd2af8d
Nick Ward <nickward13@hotmail.com>
2020-04-01 12:28:59 +1100

    added methods to archive, re-add, mark favorite and un-favorite items

--- PocketApi.UwpUnitTest/AddDeletePocketItemTest.cs
+++ PocketApi.UwpUnitTest/AddDeletePocketItemTest.cs
@@ -16,6 +16,16 @@ namespace PocketApi.UwpUnitTest
         [TestMethod]
         public async Task ExecuteTestAsync()
         {
+            PocketItem pocketItem = await TestAddAsync();
+            await TestFavoriteAsync(pocketItem);
+            await TestUnFavoriteAsync(pocketItem);
+            await TestArchiveAsync(pocketItem);
+            await TestReAddAsync(pocketItem);
+            await TestDeleteAsync(pocketItem);
+        }
+
+        private async Task<PocketItem> TestAddAsync()
+        {
             PocketItem newPocketItem = await _pocketClient.AddPocketItemAsync(new Uri("https://www.theage.com.au/culture/comedy/the-at-home-comedy-festival-20200318-p54bhu.html"));
             List<PocketItem> newPocketItems = await _pocketClient.GetPocketItemsAsync(
                 DateTime.Now.ToUniversalTime().Subtract(new TimeSpan(0, 0, 30)));
@@ -24,9 +34,36 @@ namespace PocketApi.UwpUnitTest
             Assert.IsNotNull(newPocketItem.Id);
             Assert.AreNotEqual(0, newPocketItem.Id.Length);
             Assert.AreNotEqual(0, newPocketItems.Count);
+            return newPocketItem;
+        }
 
-            bool result = await _pocketClient.DeletePocketItemAsync(
-                newPocketItem);
+        private async Task TestFavoriteAsync(PocketItem pocketItem)
+        {
+            bool result = await _pocketClient.MarkFavoritePocketItemAsync(pocketItem);
+            Assert.AreEqual(true, result);
+        }
+
+        private async Task TestUnFavoriteAsync(PocketItem pocketItem)
+        {
+            bool result = await _pocketClient.UnFavoritePocketItemAsync(pocketItem);
+            Assert.AreEqual(true, result);
+        }
+
+        private async Task TestArchiveAsync(PocketItem pocketItem)
+        {
+            bool result = await _pocketClient.ArchivePocketItemAsync(pocketItem);
+            Assert.AreEqual(true, result);
+        }
+
+        private async Task TestReAddAsync(PocketItem pocketItem)
+        {
+            bool result = await _pocketClient.ReAddPocketItemAsync(pocketItem);
+            Assert.AreEqual(true, result);
+        }
+
+        private async Task TestDeleteAsync(PocketItem pocketItem)
+        {
+            bool result = await _pocketClient.DeletePocketItemAsync(pocketItem);
 
             Assert.AreEqual(true, result);
         }
--- PocketApi/PocketClient.ModifyPocketItem.cs
+++ PocketApi/PocketClient.ModifyPocketItem.cs
@@ -12,6 +12,61 @@ namespace PocketApi
     {
         private static Uri _modifyUri = new Uri($"https://getpocket.com/v3/send");
 
+        public async Task<bool> DeletePocketItemAsync(PocketItem pocketItem)
+        {
+            PocketModifyAction pocketDeleteAction = new PocketModifyAction()
+            {
+                Action = PocketModifyActionType.Delete,
+                ItemId = pocketItem.Id
+            };
+
+            return await ModifyPocketItemAsync(pocketDeleteAction);
+        }
+
+        public async Task<bool> ArchivePocketItemAsync(PocketItem pocketItem)
+        {
+            PocketModifyAction archiveModifyAction = new PocketModifyAction()
+            {
+                Action = PocketModifyActionType.Archive,
+                ItemId = pocketItem.Id
+            };
+
+            return await ModifyPocketItemAsync(archiveModifyAction);
+        }
+
+        public async Task<bool> ReAddPocketItemAsync(PocketItem pocketItem)
+        {
+            PocketModifyAction reAddModifyAction = new PocketModifyAction()
+            {
+                Action = PocketModifyActionType.ReAdd,
+                ItemId = pocketItem.Id
+            };
+
+            return await ModifyPocketItemAsync(reAddModifyAction);
+        }
+
+        public async Task<bool> MarkFavoritePocketItemAsync(PocketItem pocketItem)
+        {
+            PocketModifyAction favoriteModifyAction = new PocketModifyAction()
+            {
+                Action = PocketModifyActionType.Favorite,
+                ItemId = pocketItem.Id
+            };
+
+            return await ModifyPocketItemAsync(favoriteModifyAction);
+        }
+
+        public async Task<bool> UnFavoritePocketItemAsync(PocketItem pocketItem)
+        {
+            PocketModifyAction unfavoriteModifyAction = new PocketModifyAction()
+            {
+                Action = PocketModifyActionType.UnFavorite,
+                ItemId = pocketItem.Id
+            };
+
+            return await ModifyPocketItemAsync(unfavoriteModifyAction);
+        }
+
         private async Task<bool> ModifyPocketItemAsync(PocketModifyAction pocketModifyAction)
         {
             string apiResponse = await ApiPostAsync(
