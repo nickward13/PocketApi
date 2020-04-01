c36d79a33676d41c4b928839b5bb774d657a2ee0
Nick Ward <nickward13@hotmail.com>
2020-04-01 11:52:30 +1100

    refactored delete and modify methods

--- PocketApi/PocketClient.DeletePocketItem.cs
+++ PocketApi/PocketClient.DeletePocketItem.cs
@@ -10,39 +10,16 @@ namespace PocketApi
 {
     public partial class PocketClient
     {
-        private static Uri _modifyUri = new Uri($"https://getpocket.com/v3/send");
-
+        
         public async Task<bool> DeletePocketItemAsync(PocketItem deletePocketItem)
         {
-            string apiResponse = await ApiPostAsync(
-                _modifyUri,
-                new ModifyPocketItemBody()
-                {
-                    ConsumerKey = _accessToken.ConsumerKey,
-                    AccessToken = _accessToken.Token,
-                    Actions = new PocketModifyAction[]
-                    {
-                        new PocketModifyAction()
-                        {
-                            Action = PocketModifyActionType.Delete,
-                            ItemId = deletePocketItem.Id
-                        }
-                    }
-                });
-
-            JsonDocument apiJsonDocument = JsonDocument.Parse(apiResponse);
-            foreach (JsonProperty property in apiJsonDocument.RootElement.EnumerateObject())
+            PocketModifyAction pocketDeleteAction = new PocketModifyAction()
             {
-                if (property.Name == "status")
-                {
-                    if (property.Value.GetDouble() != 1)
-                        return false;
-                    else
-                        return true;
-                }
-            }
+                Action = PocketModifyActionType.Delete,
+                ItemId = deletePocketItem.Id
+            };
 
-            return false;
+            return await ModifyPocketItemAsync(pocketDeleteAction);
         }
     }
 }
---
+++ PocketApi/PocketClient.ModifyPocketItem.cs
@@ -0,0 +1,44 @@
+﻿using PocketApi.Models;
+using PocketApi.RestApiRequestModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PocketApi
+{
+    public partial class PocketClient
+    {
+        private static Uri _modifyUri = new Uri($"https://getpocket.com/v3/send");
+
+        private async Task<bool> ModifyPocketItemAsync(PocketModifyAction pocketModifyAction)
+        {
+            string apiResponse = await ApiPostAsync(
+                _modifyUri,
+                new ModifyPocketItemBody()
+                {
+                    ConsumerKey = _accessToken.ConsumerKey,
+                    AccessToken = _accessToken.Token,
+                    Actions = new PocketModifyAction[]
+                    {
+                        pocketModifyAction
+                    }
+                });
+
+            JsonDocument apiJsonDocument = JsonDocument.Parse(apiResponse);
+            foreach (JsonProperty property in apiJsonDocument.RootElement.EnumerateObject())
+            {
+                if (property.Name == "status")
+                {
+                    if (property.Value.GetDouble() != 1)
+                        return false;
+                    else
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
